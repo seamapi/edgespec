dc6d774b27f2b6b9e8ab35382a07684416ef27a5
seveibar <seveibar@gmail.com>
2023-11-29 22:57:33 -0800

    refactor for better help messaging

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -6,54 +6,61 @@ import { createServerFromDir } from "../serve/create-server-from-dir.js"
 import { generateModuleCode } from "../codegen/generate-module-code.js"
 import fs from "fs"
 
-const argv = yargs(hideBin(process.argv))
+const parseRes = yargs(hideBin(process.argv))
   .scriptName("edgespec")
   .usage("$0 <cmd> [args]")
-  .command(
-    "serve",
-    "start the server",
-    (yargs) => {
-      return yargs.positional("dir", {}).option("port", {
-        default: "3000",
+  .command("serve", "start the server", (yargs) => {
+    return yargs.positional("dir", {}).option("port", {
+      default: "3000",
+      alias: "p",
+      type: "number",
+    })
+  })
+  .command("build [project_type]", "build a project", (yargs) => {
+    return yargs
+      .option("project_type", {
         alias: "p",
-        type: "number",
+        describe: "Type of project to build",
+        choices: ["route-map", "module", "next", "hono", "deno", "bun"],
       })
-    },
-    async (argv) => {
-      console.log(
-        `Starting edgespec server on http://localhost:${argv.port}...`
-      )
-      const server = await createServerFromDir(argv._[1] ?? "./api")
-      server.listen(argv.port ?? 3000)
+      .option("routes", {
+        alias: "r",
+        description: "Routes directory (e.g. ./routes)",
+      })
+      .option("out-file", {
+        alias: "o",
+        description: "Output file (e.g. ./module.ts or ./route-map.ts)",
+      })
+  })
+
+async function main() {
+  const { argv } = parseRes
+  if (argv._[0] === "build") {
+    console.log(`Building project: ${argv.project_type}`)
+    if (argv.project_type === "route-map" || argv.project_type === "module") {
+      const code = await generateModuleCode(argv.routes ?? "./routes")
+      fs.writeFileSync(argv.outFile ?? "./module.ts", code)
+      process.exit(0)
     }
-  )
-  .command(
-    "build [project_type]",
-    "build a project",
-    (yargs) => {
-      return yargs
-        .option("project_type", {
-          alias: "p",
-          describe: "Type of project to build",
-          choices: ["route-map", "module", "next", "hono", "deno", "bun"],
-        })
-        .option("routes", {
-          alias: "r",
-          description: "Routes directory (e.g. ./routes)",
-        })
-        .option("out-file", {
-          alias: "o",
-          description: "Output file (e.g. ./module.ts or ./route-map.ts)",
-        })
-    },
-    async (argv) => {
-      console.log(`Building project: ${argv.project_type}`)
-      if (argv.project_type === "route-map" || argv.project_type === "module") {
-        const code = await generateModuleCode(argv.routes ?? "./routes")
-        fs.writeFileSync(argv.outFile ?? "./module.ts", code)
-        return
-      }
-      throw new Error(`Unimplemented project type: "${argv.project_type}"`)
+    throw new Error(`Unimplemented project type: "${argv.project_type}"`)
+  } else if (argv._[0] === "serve") {
+    console.log(`Starting edgespec server on http://localhost:${argv.port}...`)
+    const server = await createServerFromDir(argv._[1] ?? "./api")
+    console.log("listening...")
+    server.listen(argv.port ?? 3000)
+    console.log("waiting...")
+    while (true) {
+      // TODO check if server running
+      await new Promise((resolve) => setTimeout(resolve, 10))
     }
+  } else {
+    parseRes.showHelp()
+  }
+}
+
+main().catch((e) => {
+  console.error(
+    `Error while running edgespec CLI: ${e.toString()}\n\n${e.stack}`
   )
-  .help().argv
+  process.exit(1)
+})
