7f4b79669375c0cabb8463972e84e2320b9e19d4
Max Isom <hi@maxisom.me>
2023-12-07 17:05:28 -0800

    Extract bundling function

--- src2/tests/fixture.ts
+++ src2/tests/fixture.ts
@@ -5,6 +5,8 @@ import esbuild from "esbuild"
 import * as edge from "edge-runtime"
 import { createRouteMapFromDirectory } from "src2/routes/create-route-map-from-directory.ts"
 import { generateWinterCGMinimalEntry } from "src2/targets/entries/wintercg-minimal"
+import { bundle } from "src2/bundle/bundle"
+import fs from "node:fs/promises"
 
 interface StartTestFixtureFromDirectoryOptions {
   directoryPath: string
@@ -20,35 +22,40 @@ export const startTestFixtureFromDirectory = async ({directoryPath, port}: Start
     req.on("data", (chunk) => chunks.push(chunk))
     await once(req, "end")
     const body = Buffer.concat(chunks)
-    const result = await esbuild.build({
-      stdin: {
-        contents: generateWinterCGMinimalEntry(routeMap),
-        resolveDir: directoryPath,
-        loader: "ts",
-      },
-      bundle: true,
-      format: "esm",
-      write: false
-    })
 
-    const runtime = new edge.EdgeRuntime({
-      initialCode: result.outputFiles[0].text,
-    })
+    try {
+      const bundled = await bundle({
+        routeMap,
+        directoryPath,
+        bundledAdapter: "wintercg-minimal"
+      })
 
-    const fullUrl = `http://localhost:${req.socket.localPort}${req.url}`
+      const runtime = new edge.EdgeRuntime({
+        initialCode: bundled,
+      })
 
-    const response = await runtime.dispatchFetch(fullUrl, {
-      method: req.method,
-      headers: Object.entries(req.headers) as [string, string][],
-      body: req.method === "GET" ? undefined : body,
-    })
-    await response.waitUntil()
+      const fullUrl = `http://localhost:${req.socket.localPort}${req.url}`
 
-    res.statusCode = response.status
-    response.headers.forEach((value, key) => {
-      res.setHeader(key, value)
-    })
-    res.end(await response.text())
+      const response = await runtime.dispatchFetch(fullUrl, {
+        method: req.method,
+        headers: Object.entries(req.headers) as [string, string][],
+        body: req.method === "GET" ? undefined : body,
+      })
+      await response.waitUntil()
+
+      res.statusCode = response.status
+      response.headers.forEach((value, key) => {
+        res.setHeader(key, value)
+      })
+      res.end(await response.text())
+    } catch (error) {
+      res.statusCode = 500
+      res.end(JSON.stringify({
+        errorType: "emulated_wintercg_runtime_error",
+        errorMessage: (error as Error).message,
+        errorStack: (error as Error).stack,
+      }))
+    }
   })
   server.listen(port)
   await once(server, "listening")
