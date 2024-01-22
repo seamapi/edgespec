f900349a5c9c3f6d055df462aed86c9b7b9b0a6e
Max Isom <hi@maxisom.me>
2024-01-22 12:19:48 -0800

    Fix test for Node v18?

--- tests/fixtures/get-test-cli.ts
+++ tests/fixtures/get-test-cli.ts
@@ -4,9 +4,13 @@ import { execa } from "execa"
 export const getTestCLI = async (t: ExecutionContext) => {
   return {
     executeCommand: async (args: string[]) => {
-      const result = await execa("tsx", ["src/cli/cli.ts", ...args], {
-        reject: false,
-      })
+      const result = await execa(
+        "node",
+        ["--import=tsx", "src/cli/cli.ts", ...args],
+        {
+          reject: false,
+        }
+      )
       t.log(result.stderr)
       t.log(result.stdout)
       return result
