248a4aa722c840e7086b1a946a46e0fa7692a0e0
Max Isom <hi@maxisom.me>
2024-01-22 11:55:35 -0800

    Add new CLI framework & bundle command

---
+++ src/cli/cli.ts
@@ -0,0 +1,12 @@
+import { runExit } from "clipanion"
+import { BundleCommand } from "./commands/bundle"
+import { version } from "../../package.json"
+
+runExit(
+  {
+    binaryLabel: "EdgeSpec",
+    binaryName: "edgespec",
+    binaryVersion: version,
+  },
+  [BundleCommand]
+)
