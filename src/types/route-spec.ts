5649c324c09ee6b85eefe68177ed4ddc146da5ff
charburgx <charburgx@gmail.com>
2024-02-01 12:45:21 -0800

    remove sdkreturnvalue

--- src/types/route-spec.ts
+++ src/types/route-spec.ts
@@ -32,10 +32,6 @@ export type RouteSpec<AuthMiddlewares extends string> = {
   auth: AuthMiddlewares | readonly AuthMiddlewares[] | "none"
   middlewares?: MiddlewareChain
 
-  /**
-   * add x-fern-sdk-return-value to the openapi spec, useful when you want to return only a subset of the response
-   */
-  sdkReturnValue?: string | string[]
   openApiMetadata?: any
 
   onMultipleAuthMiddlewareFailures?: (errors: unknown[]) => void
