28693674f60b9798dd0b5281972d1985519b8446
Max Isom <hi@maxisom.me>
2024-02-05 13:53:02 -0800

    Remove old files

--- src/build/load-api-directory-as-vfs.js
+++
@@ -1,5 +0,0 @@
-import { getVirtualFileSystemFromDirPath } from "make-vfs"
-
-export const loadApiDirectoryAsVfs = (dirPath) => {
-  return getVirtualFileSystemFromDirPath(dirPath)
-}
--- tests/codegen/build-module-service.test.ts
+++
@@ -1,9 +0,0 @@
-import { generateModuleCode } from "src/codegen/generate-module-code.js"
-import test from "ava"
-
-test("test generating module code", async (t) => {
-  const code = await generateModuleCode("./examples/hello-world/routes")
-  t.truthy(code.includes("export const routeMap"))
-  t.truthy(code.includes("export const GeneratedService"))
-  t.truthy(code.includes("new ModuleService(routeMap)"))
-})
