72eca9a1e7c79f6c92a74d44125234a70cb99822
Max Isom <hi@maxisom.me>
2024-02-08 12:57:06 -0800

    Export fixture

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,13 +2,18 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   entry: {
+    // Base library
     index: "src/index.ts",
+    middleware: "src/middleware/index.ts",
+    // Adapters
     "adapters/node": "src/adapters/node.ts",
     "adapters/wintercg-minimal": "src/adapters/wintercg-minimal.ts",
+    // Programmatic usage
     cli: "src/cli/cli.ts",
     dev: "src/dev/dev.ts",
-    middleware: "src/middleware/index.ts",
     config: "src/config/index.ts",
+    // Test fixtures
+    "testing/ava": "src/testing/ava/index.ts",
   },
   format: "esm",
   treeshake: true,
