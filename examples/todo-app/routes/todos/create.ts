2d490152dbb536af0ce4dfc7c9fe3fd3861f8688
Severin Ibarluzea <seve@seve-squid.local>
2024-02-11 14:32:24 -0800

    wip

---
+++ examples/todo-app/routes/todos/create.ts
@@ -0,0 +1,8 @@
+import { withEdgeSpec } from "../../src/with-edge-spec"
+
+export default withEdgeSpec({
+  auth: "none",
+  methods: ["GET"],
+})((req) => {
+  return new Response("Hello world!")
+})
