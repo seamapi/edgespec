db8b3c7a737a266eb1ea98d7857db96e230e0161
Severin Ibarluzea <seve@seve-squid.local>
2024-02-11 21:35:24 -0800

    add node compat

--- src/bundle/bundle.ts
+++ src/bundle/bundle.ts
@@ -1,8 +1,22 @@
 import esbuild from "esbuild"
 import { constructManifest } from "./construct-manifest"
 import { BundleOptions } from "./types"
+import { join as joinPath } from "node:path"
+import { readFile } from "node:fs/promises"
 
 export const bundle = async (options: BundleOptions) => {
+  const rootDirPackageJson = await readFile(
+    joinPath(options.rootDirectory, "package.json")
+  )
+    .then((r) => JSON.parse(r.toString()))
+    .catch((e) => {
+      throw new Error(
+        `Could not read package.json in ${
+          options.rootDirectory
+        }\n\n${e.toString()}`
+      )
+    })
+
   const result = await esbuild.build({
     stdin: {
       contents: await constructManifest(options),
@@ -12,7 +26,9 @@ export const bundle = async (options: BundleOptions) => {
     bundle: true,
     format: "esm",
     write: false,
+    platform: "node",
     sourcemap: "inline",
+    external: Object.values(rootDirPackageJson.dependencies || {}),
     ...options.esbuild,
   })
 
