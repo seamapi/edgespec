1df1d8e7e63313c067895208c89a3e72ff7e2348
seveibar <seveibar@gmail.com>
2024-02-12 20:32:34 -0800

    hoist node options

--- src/bundle/bundle.ts
+++ src/bundle/bundle.ts
@@ -5,18 +5,6 @@ import { join as joinPath } from "node:path"
 import { readFile } from "node:fs/promises"
 
 export const bundle = async (options: BundleOptions) => {
-  const rootDirPackageJson = await readFile(
-    joinPath(options.rootDirectory, "package.json")
-  )
-    .then((r) => JSON.parse(r.toString()))
-    .catch((e) => {
-      throw new Error(
-        `Could not read package.json in ${
-          options.rootDirectory
-        }\n\n${e.toString()}`
-      )
-    })
-
   const result = await esbuild.build({
     stdin: {
       contents: await constructManifest(options),
@@ -26,9 +14,7 @@ export const bundle = async (options: BundleOptions) => {
     bundle: true,
     format: "esm",
     write: false,
-    platform: "node",
     sourcemap: "inline",
-    external: Object.values(rootDirPackageJson.dependencies || {}),
     ...options.esbuild,
   })
 
