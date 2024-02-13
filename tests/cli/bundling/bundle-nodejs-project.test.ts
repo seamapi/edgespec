04507d431c9290f768cb7dd1b736912c23b3cf32
Severin Ibarluzea <seveibar@gmail.com>
2024-02-13 12:40:39 -0800

    Add Node Compatibility (#78)
    Co-authored-by: Severin Ibarluzea <seve@seve-squid.local>

---
+++ tests/cli/bundling/bundle-nodejs-project.test.ts
@@ -0,0 +1,27 @@
+import test from "ava"
+import { getTestCLI } from "tests/fixtures/get-test-cli"
+import os from "node:os"
+import path from "node:path"
+import { randomUUID } from "node:crypto"
+import { fileURLToPath } from "node:url"
+
+test("edgespec bundle doesn't throw for node:* imports when --platform node is provided", async (t) => {
+  const cli = await getTestCLI(t)
+
+  const tempPath = path.join(os.tmpdir(), `${randomUUID()}.js`)
+  const appDirectoryPath = path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "./nodejs-edgespec-project/api"
+  )
+  const execution = cli.executeCommand([
+    "bundle",
+    "--platform",
+    "node",
+    "-o",
+    tempPath,
+    "--routes-directory",
+    appDirectoryPath,
+  ])
+  const result = await execution.waitUntilExit()
+  t.is(result.exitCode, 0)
+})
