5125c87415b5fa772991c3219dd96820fd39d7a4
seveibar <seveibar@gmail.com>
2024-02-21 21:18:40 -0800

    add a dynamic require to emulate nodejs

--- tests/testing/ava/node/api/random.ts
+++ tests/testing/ava/node/api/random.ts
@@ -5,5 +5,8 @@ export default withRouteSpec({
   methods: ["GET"],
   auth: "none",
 })(() => {
+  // Use a dynamic require
+  require("node:events")
+
   return new Response(crypto.randomBytes(16).toString("hex"))
 })
