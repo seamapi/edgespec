2e7460ab273952dfb4e77e213f4750cf7fdf3507
Severin Ibarluzea <seve@seve-squid.local>
2024-02-22 16:27:08 -0800

    add comment explaining why it fails

--- tests/testing/ava/node-with-pg/fixture.test.ts
+++ tests/testing/ava/node-with-pg/fixture.test.ts
@@ -4,6 +4,9 @@ import path from "node:path"
 import { getTestServer } from "src/testing/ava/fixture.ts"
 
 // TODO fix this test!
+// Fails with:
+// Unhandled exception:
+// Error: Dynamic require of "events" is not supported
 test.skip("pg can be used inside of edgespec", async (t) => {
   const { port } = await getTestServer(t, {
     rootDirectory: path.dirname(fileURLToPath(import.meta.url)),
