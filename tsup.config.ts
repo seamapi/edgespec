f5ea219de18b4df527149f9f6fca05d8dfbd4e9e
Louis Pré <louis@getseam.com>
2024-09-12 10:25:22 -0700

    feat: Add more build entries

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,14 +3,19 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   tsconfig: "tsconfig.build.json",
   entry: [
+    // Base library
     "src/index.ts",
-    "src/config/index.ts",
-    "src/dev/dev.ts",
-    "src/config/index.ts",
     "src/middleware/index.ts",
+    // Adapters
     "src/adapters/node.ts",
     "src/adapters/wintercg-minimal.ts",
+    // Programmatic usage
+    "src/cli/cli.ts",
+    "src/dev/dev.ts",
+    "src/config/index.ts",
+    // Test fixtures
     "src/testing/ava/index.ts",
+    "src/testing/ava/worker-wrapper.ts",
   ],
   format: ["esm", "cjs"],
   outDir: "dist",
