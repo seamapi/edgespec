37e076ccc6a30bf7549e6efc10a55bd44088854b
Louis Pré <louis@getseam.com>
2024-09-12 10:19:38 -0700

    feat: Use tsup to manage all build outputs

--- src/commonjs.ts
+++
@@ -1,9 +0,0 @@
-export * from "./index.js"
-
-// Additional exports to include in CJS bundle
-export * from "./config/index.js"
-export * from "./dev/dev.js"
-export * from "./middleware/index.js"
-export * from "./adapters/node.js"
-export * from "./adapters/wintercg-minimal.js"
-export * from "./testing/ava/index.js"
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  tsconfig: "tsconfig.build.cjs.json",
-  entry: ["src/commonjs.ts"],
-  format: ["cjs"],
-  outDir: "dist/cjs",
+  tsconfig: "tsconfig.build.json",
+  entry: [
+    "src/index.ts",
+    "src/config/index.ts",
+    "src/dev/dev.ts",
+    "src/config/index.ts",
+    "src/middleware/index.ts",
+    "src/adapters/node.ts",
+    "src/adapters/wintercg-minimal.ts",
+    "src/testing/ava/index.ts",
+  ],
+  format: ["esm", "cjs"],
+  outDir: "dist",
+  sourcemap: true,
+  dts: true,
   treeshake: true,
   clean: true,
+  external: ["ava", "ava/plugin", "ava-typescript-worker"],
 })
